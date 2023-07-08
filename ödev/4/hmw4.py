764b9dc80498109604693638c757b57422e9f090
Ekrem <ekrem.oncu18@gmail.com>
2023-07-08 22:47:12 +0300

    ödev 4 update

--- ödev/4/hmw4.py
+++ ödev/4/hmw4.py
@@ -9,7 +9,7 @@ while True:
         print(s, end="")
 """       
  
-# Solution 3
+# Solution 3 ?
 """
 n = int (input("Bir sayi gir: "))
 l = [[0] * n for _ in range(n)]
@@ -31,7 +31,7 @@ p.remove("-")
 print(p)
 """
 
-# Solution 5
+# Solution 5 
 """
 n = int(input("Bir sayi gir: "))
 
@@ -41,6 +41,7 @@ for i in range(n+1):
 
 """
 # Solution 6
+"""   
 a = input("Bir yazi gir: ")
 count_lower = 0
 count_upper = 0
@@ -52,24 +53,53 @@ for i in a:
             count_upper += 1
 print(f" {count_lower} tane küçük harf vardır")                
 print(f" {count_upper} tane küçük harf vardır")     
+"""
+# Solution 7
+"""
+b = input("Bir yazi gir: ")
+a = []  
 
+for i in range(len(b)):
+    c = b.count(b[i])
+    a.append([b[i],c])
 
+d = dict(a)
+print(d)
+"""    
+# Solution 8
+"""
+a = [12, 56, 89, 32, 19, 99, 43]
 
+for i in range(len(a)):
+    a[i] = a[i] // 10 + (a[i] % 10) *10
+print(a)
+"""
+# Solution 9
+"""
+width = int(input("genişlik gir: "))
+height = int(input("yükseklik gir: "))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(height):
+    print("|" + i*" " + '*' + (height - i) *" " + '|')
+for i in range(height):
+    print("|" + (height - i)*" " + '*' +  i*" " + '|')
+for i in range(height):
+    print("|" + i*" " + '*' + (height - i) *" " + '|')
+"""
+# Solution 10
+n = int(input("n sayisi gir: "))
+t = 1
+for _ in range(n):
+    m = t * '*'
+    k = m.center(n*2 -1)
+    print(k)
+    t +=2
+t-=2    
+for _ in range(n):
+    m = t * '*'
+    k = m.center(n*2 -1)
+    print(k)
+    t -=2
 
 
 
