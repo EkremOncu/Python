89225a56c807075832c4ae149adb36ef7f70750b
Ekrem <ekrem.oncu18@gmail.com>
2023-07-23 02:10:54 +0300

    hmw5 continue...

---
+++ ödev/5/hmw5.py
@@ -0,0 +1,66 @@
+# Solution 1 
+def disp_shape(n):
+    k = 1
+    s = 0 
+    for i in a:
+        for i in range(k, n+1):
+            print(i, end=" ")
+            
+            
+        print(" ")
+        print(" " * s, end=" ")
+        k = k+1
+        s = s+1
+    
+    t = 0
+    r = 0
+    for i in a:
+        for i in range(n, n-1+t, -1):
+            print(i, end=" ")  
+        
+        print(" ")
+  #      print(" " * r, end=" ")
+        t = t-1
+ #       r = r+1
+              
+       
+
+n = int(input("Bir sayi giriniz: "))
+a = list(range(1,n+1))
+disp_shape(n)
+
+
+
+# Solution 2
+
+# Solution 3 
+
+
+# Solution 4  
+
+
+# Solution 5 
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
