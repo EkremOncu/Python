cbc6e69c1fcaf77bf3050c9379d24aee55b7aba7
Ekrem <ekrem.oncu18@gmail.com>
2023-09-14 21:02:19 +0300

    hmw7.py update

--- ödev/7/hmw7.py
+++ ödev/7/hmw7.py
@@ -1,3 +1,22 @@
 # Solution 1   
+"""
+x= 'bugün hava çok güzeldi.'
+
+print({name for name in x if ("a" or "e" or "o" or "u" or "ü") in name})
+
+"""
+# Solution 2
+
+def sing(r):
+    for t in r:
+        if t%2 == 0:
+            print((t, "çift"))
+        else:
+            print((t, "tek"))
 
 
+a = [1, 2, 3, 4, 5]
+result = sing(a)
+
+# print([sayi for sayi in a if ])
+    
