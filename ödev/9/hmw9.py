ac0aea579b843ea34d93f01c36f2fd644d33d3d5
Ekrem <ekrem.oncu18@gmail.com>
2023-12-02 20:40:16 +0300

    hmw9 update...

--- ödev/9/hmw9.py
+++ ödev/9/hmw9.py
@@ -1,2 +1,16 @@
 # Solution 1
 
+class CommandPrompt:
+    def __init__(self,prompt):
+        self.prompt = prompt
+
+    def run(self):
+        while True:
+            prompt = input('write: ')
+            print(prompt[::-1])
+
+            if prompt == 'quit':
+                return False
+
+cp = CommandPrompt('CSD')
+cp.run()
