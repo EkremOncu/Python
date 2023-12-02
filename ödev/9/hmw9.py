68fd16f165513f1a8a1740e5c3eab6b7d08d42a0
Ekrem <ekrem.oncu18@gmail.com>
2023-12-02 22:09:53 +0300

    hmw9 update...

--- ödev/9/hmw9.py
+++ ödev/9/hmw9.py
@@ -1,4 +1,4 @@
-# -------- Solution 1 --------
+# -------------------- Solution 1 --------------------
 """
 class CommandPrompt:
     def __init__(self,prompt):
@@ -16,8 +16,9 @@ cp = CommandPrompt('CSD')
 cp.run()
 """
 
-# -------- Solution 2 --------
 
+# -------------------- Solution 2 --------------------
+"""
 class Rectangle:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
@@ -32,7 +33,7 @@ class Rectangle:
             return False
 
     def intersect(self, rect):
-        pass
+       pass
 
     def disp(self):
         print(f'x1 = {self.x1}, y1 = {self.y1}, x2 = {self.x2}, y2 = {self.y2}')
@@ -50,12 +51,37 @@ r2 = Rectangle(15, 15, 30, 30)
 #r3 = r1.intersect(r2)
 #r3.disp()
 
-
-# -------- Solution 3 --------
-
+"""
 
 
+# -------------------- Solution 3 --------------------
+"""
+class Circle:
+    def __init__(self, *, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
 
+    def disp(self):
+        print(f'center = ({self.x}, {self.y}), radius = {self.r}')
+
+    def move(self, xoff, yoff):
+        self.x = self.x + xoff
+        self.y = self.y + yoff
+
+    def isinside(self, x, y):
+        if x>= self.x-self.r and x<= self.x+self.r and y>= self.y-self.r and y<= self.y+self.r:
+            return True
+        else:
+            return False
+
+c = Circle(x= 10, y= 12, r= 5)
+c.disp()
+c.move(-2, 4)
+c.disp()
+print('İçinde' if c.isinside(5, 7) else 'İçinde Değil')
+
+"""
 
 
 
