96382ba94f9af567e8c70572831532b63be90885
Ekrem <ekrem.oncu18@gmail.com>
2023-12-04 00:36:11 +0300

    hmw10 updates ...

--- ödev/10/hmw10.py
+++ ödev/10/hmw10.py
@@ -7,10 +7,11 @@ class Time:
         self._tseconds = hour*3600 + minute*60 + second
 
     def __repr__(self):
-        print(f"{self.hour:02d}:{self.minute:02d}:{self.second:02d} - 0'dan geçen saniye sayısı= {self._tseconds}")
+        return f"{self.hour:02d}:{self.minute:02d}:{self.second:02d} - 0'dan geçen saniye sayısı= {self._tseconds}"
 
     def __str__(self):
-        print(f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}')
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
 
     def _total_seconds(self):
         _tseconds = self.hour * 3600 + self.minute * 60 + self.second
@@ -30,67 +31,84 @@ class Time:
                 hour = 0
                 minute = 0
                 second = seconds
-        self.hour = hour
-        self.minute = minute
-        self.second = second
-        return print(self.hour, self.minute, self.second)
 
-        def __lt__(self, t):
-            if isinstance(t, int | float):
-                result = self._tseconds < t
-            elif isinstance(t, Time):
-                result = self._tseconds < t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+        return Time(hour,minute,second)
 
-        def __le__(self, t):
-            if isinstance(t, int | float):
-                result = self._tseconds <= t
-            elif isinstance(t, Time):
-                result = self._tseconds <= t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+    def __lt__(self, t):
+        if isinstance(t, int | float):
+            result = self._tseconds < t
+        elif isinstance(t, Time):
+            result = self._tseconds < t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
 
-        def __gt__(self, t):
-            if isinstance(t, int|float):
-                result= self._tseconds > t
-            elif isinstance(t, Time):
-                result= self._tseconds > t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+    def __le__(self, t):
+        if isinstance(t, int | float):
+            result = self._tseconds <= t
+        elif isinstance(t, Time):
+            result = self._tseconds <= t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
 
-        def __ge__(self, t):
-            if isinstance(t, int | float):
-                result = self._tseconds >= t
-            elif isinstance(t, Time):
-                result = self._tseconds >= t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+    def __gt__(self, t):
+        if isinstance(t, int|float):
+            result= self._tseconds > t
+        elif isinstance(t, Time):
+            result= self._tseconds > t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
 
-        def __eq__(self, t):
-            if isinstance(t, int | float):
-                result = self._tseconds == t
-            elif isinstance(t, Time):
-                result = self._tseconds == t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+    def __ge__(self, t):
+        if isinstance(t, int | float):
+            result = self._tseconds >= t
+        elif isinstance(t, Time):
+            result = self._tseconds >= t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
 
-        def __ne__(self, t):
-            if isinstance(t, int | float):
-                result = self._tseconds != t
-            elif isinstance(t, Time):
-                result = self._tseconds != t._tseconds
-            else:
-                raise TypeError('invalid type')
-            return result
+    def __eq__(self, t):
+        if isinstance(t, int | float):
+            result = self._tseconds == t
+        elif isinstance(t, Time):
+            result = self._tseconds == t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
+
+    def __ne__(self, t):
+        if isinstance(t, int | float):
+            result = self._tseconds != t
+        elif isinstance(t, Time):
+            result = self._tseconds != t._tseconds
+        else:
+            raise TypeError('invalid type')
+        return result
+
+    def __add__(self,t):
+        if isinstance(t, int | float):
+            result= self._tseconds + t
+        elif isinstance(t, Time):
+            result = self._tseconds + t._tseconds
+        else:
+            raise TypeError('invalid type')
+
+        return self._seconds_to_time(result)
+
+    __radd__ = __add__
 
 dc = Time(5,8,4)
+ac = Time(4,2,3)
+"""
 dc.__repr__()
 dc.__str__()
+"""
+print (dc._seconds_to_time(dc._tseconds))
+
 dc._total_seconds()
-dc._seconds_to_time(dc._tseconds)
+ac._total_seconds()
+
+print(dc+ac, type(dc+ac))
