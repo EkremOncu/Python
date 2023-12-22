6465d858a12b291ccff5994b8a6ff70dac9a68a8
Ekrem <ekrem.oncu18@gmail.com>
2023-12-22 20:48:59 +0300

    lecture.py up...

--- lecture.py
+++ lecture.py
@@ -5023,9 +5023,31 @@ print('------------')
 
 print(len(mr))          
 ------------------------------------------------------------------------------------
+Built-in enumerate fonksiyonunu biz de basit bir biçimde aşağıdaki gibi yazabiliriz.
+
+class myenumerate:
+    def __init__(self, iterable, start = 0):
+        self.iterator = iterable.__iter__()
+        self.i = start
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.i += 1
+        return self.i - 1, self.iterator.__next__()
+    
+a = ['ali', 'veli', 'selami', 'ayşe', 'fatma']
+
+me = myenumerate(a, 10)
+
+for index, name in me:
+    print(index, name)
+    
+for index, name in me:
+    print(index, name)    
+------------------------------------------------------------------------------------
 """
-
-
 
 
 
