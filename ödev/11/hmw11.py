2b444caca44fc28830147a9ade9289e97b8a7c85
Ekrem <ekrem.oncu18@gmail.com>
2023-12-26 17:33:13 +0300

    hmw11 update

--- ödev/11/hmw11.py
+++ ödev/11/hmw11.py
@@ -1,18 +1,75 @@
-# Solution 1
 with open('teste.txt', 'w+', encoding='utf-8') as f:
-    f.write("LeBron James(goat)\nGabe Vincent(Heat culture and injured)\nAustin Reeves(I'm him)\nRui Hachimura(Japanese)\nAnthony Davis(Defensive player of the year)")
+    f.write("LeBron James(goat)\nGabe Vincent(Heat culture and injured)\nAustin Reeves(I'm him)\nRui Hachimura(Japanese)\nAnthony Davis Defensive player of the year")
     f.seek(0)
     s= f.read()
-    print(s)
 print()
-def mygrep(text):
+
+# Solution 1
+"""
+def mygrep(path,text):
+    
+    with open(path, 'r', encoding='utf-8') as f:
+        s= f.read()
+        print(s)
+        print()
+        
     res= s.split('\n')
     found= [index for index, item in enumerate(res) if text in item]
+    
     if found:
         for i in range(len(found)):
             print(f"<{found[i]}>: {res[found[i]]}")
 
-mygrep('him')
+mygrep('teste.txt','him')
+"""
+# Solution 2
+"""
+def disp_last_lines(path,n):
+    
+    with open(path, 'r', encoding='utf-8') as f:
+        s= f.read()
+        print(s)
+        print()
+        
+    res= s.split('\n')
+    found= [index for index,item in enumerate(res)]
+    
+    for i in range(len(found)-1,len(found)-n-1,-1):
+        print(f'{res[i]}')    
+       
+disp_last_lines('teste.txt', 3)
+"""
+# Solution 3
+"""
+def get_count(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        s = f.read()
+        print(s)
+        print()
+
+    res= s.split('\n')
+    lcount= len(res)
 
 
+    ccount= 0
+    for i in range(len(res)):
+        ccount += len(res[i])
+    ccount += len(res)-1
+
+
+    a= s.replace('\n', '')
+    a= a.replace('(', ' ')
+    a= a.replace(')', ' ')
+    a= a.replace("'", ' ')
+    a= a.split(' ')
+    wcount= len(a)
+
+    return lcount,wcount,ccount
+
+
+lcount,wcount,ccount= get_count('teste.txt')
+print(lcount,wcount,ccount)
+"""
+
+# Solution 4
 
