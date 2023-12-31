edd9a8df7af99a48730bd11401ebed9af4637010
Ekrem <ekrem.oncu18@gmail.com>
2023-12-31 17:43:31 +0300

    hmw12 up.

--- ödev/12/hmw12.py
+++ ödev/12/hmw12.py
@@ -12,18 +12,19 @@ class Mycount:
         self.i += self.step
         return self.i - self.step
 
-def mycount(start, step=1):
-    while True:
-        yield start
-        start = start + step
+iterator1 = Mycount(10,2)
 
-iterator = Mycount(10,2)
-
-for index, x in enumerate(iterator):
+for index, x in enumerate(iterator1):
     if index == 100:
         break
     print(x, end=' ')
 print()
+
+
+def mycount(start, step=1):
+    while True:
+        yield start
+        start = start + step
 
 iterator = mycount(10,2)
 
@@ -34,3 +35,41 @@ for index, x in enumerate(iterator):
 """
 
 # Solution 2
+"""
+class Mycycle:
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.iterator = self.iterable.__iter__()
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return self.iterator.__next__()
+        except StopIteration:
+            self.iterator = self.iterable.__iter__()  # Yeni iterator
+            return self.iterator.__next__()
+
+a = ['A', 'B', 'C']
+
+for index, x in enumerate(Mycycle(a)):
+    if index == 100:
+        break
+    print(x, end=' ')
+print()
+
+
+def mycycle(iterable):
+    while True:
+        for i in iterable:
+            yield i
+
+a = ['A', 'B', 'C']
+
+for index, x in enumerate(mycycle(a)):
+    if index == 100:
+        break
+    print(x, end=' ')
+"""
+
+# Solution 3
