64ea5334d0d98606ab8a338ecefa6cae3c37b717
Ekrem <ekrem.oncu18@gmail.com>
2024-01-09 05:15:08 +0300

    hmw11 and hmw12 adjusment

--- ödev/11/hmw11.py
+++ ödev/11/hmw11.py
@@ -94,7 +94,7 @@ def copy_file(source_path, dest_path):
 
     with open(source_path, 'r') as f:
         while True:
-                s = f.read(1024)
+                s = f.read(10)
                 q.write(s)
                 if s == '':
                     break
@@ -111,42 +111,3 @@ def copy_file(source_path, dest_path):
 
 copy_file ('teste.txt', 'testecopy.txt')
 """
-# Solution 6   # eksik
-
-def read_csv(path, sep=',', skiprows=0, converter={}):
-    with open(path,'r') as f:
-        s= f.read()
-
-        les = s.split(sep)
-
-        s= s.replace('\n', sep)
-        res = s.split(sep)
-
-        flag = False
-        s = 0
-        for i in les:
-            flag = '\n' in i
-            s += 1
-            if flag:
-                break
-
-        matris = [[] for _ in range(len(res) // s)]
-
-        q = 0
-        for i in range(len(matris)):
-            for _ in range(s):
-                matris[i].append(res[q])
-                q += 1
-
-        del matris[0:skiprows]
-
-    return matris
-
-
-result = read_csv("FB.csv", ';', 1)
-
-print(result)
-print()
-for satir in result:
-    print(satir)
-
--- ödev/12/hmw12.py
+++ ödev/12/hmw12.py
@@ -1,164 +1,63 @@
 # Solution 1
 """
-class Mycount:
-    def __init__(self, start, step=1):
-        self.start = start
-        self.step = step
-        self.i = self.start
+def disp_last_lines(path,n):
 
-    def __iter__(self):
-        return self
+    with open(path, 'r', encoding='utf-8') as f:
+        s= f.read()
+        print(s)
+        print()
 
-    def __next__(self):
-        self.i += self.step
-        return self.i - self.step
+    res= s.split('\n')
+    found= [index for index,item in enumerate(res)]
 
-iterator1 = Mycount(10,2)
+    for i in range(len(found)-1,len(found)-n-1,-1):
+        print(f'{res[i]}')
 
-for index, x in enumerate(iterator1):
-    if index == 100:
-        break
-    print(x, end=' ')
+disp_last_lines('teste.txt', 2)
+"""
+
+# Solution 2   # eksik
+
+def read_csv(path, sep=',', skiprows=0, converter={}):
+    with open(path,'r') as f:
+        s= f.read()
+
+        les = s.split(sep)
+
+        s= s.replace('\n', sep)
+        res = s.split(sep)
+
+        flag = False
+        s = 0
+        for i in les:
+            flag = '\n' in i
+            s += 1
+            if flag:
+                break
+
+        matris = [[] for _ in range(len(res) // s)]
+
+        q = 0
+        for i in range(len(matris)):
+            for _ in range(s):
+                matris[i].append(res[q])
+                q += 1
+
+        del matris[0:skiprows]
+
+    return matris
+
+
+result = read_csv("FB.csv", ';', 1)
+
+print(result)
 print()
+for satir in result:
+    print(satir)
 
 
-def mycount(start, step=1):
-    while True:
-        yield start
-        start = start + step
-
-iterator = mycount(10,2)
-
-for index, x in enumerate(iterator):
-    if index == 100:
-        break
-    print(x, end=' ')
-"""
-
-# Solution 2
-"""
-class Mycycle:
-    def __init__(self, iterable):
-        self.iterable = iterable
-        self.iterator = self.iterable.__iter__()
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        try:
-            return self.iterator.__next__()
-        except StopIteration:
-            self.iterator = self.iterable.__iter__()  # Yeni iterator
-            return self.iterator.__next__()
-
-a = ['A', 'B', 'C']
-
-for index, x in enumerate(Mycycle(a)):
-    if index == 100:
-        break
-    print(x, end=' ')
-print()
 
 
-def mycycle(iterable):
-    while True:
-        for i in iterable:
-            yield i
-
-a = ['A', 'B', 'C']
-
-for index, x in enumerate(mycycle(a)):
-    if index == 100:
-        break
-    print(x, end=' ')
-"""
-
-# Solution 3
-"""
-???????????????????????????????????????????????????????????????????????????????
-class Myislice:
-    def __init__(self, iterable, start, stop=None, step=1):
-        self.iterable = iterable
-        self.iterator = self.iterable.__iter__()
-        self.step = step
-        self.stop = stop
-        self.i = 0
-
-        if self.stop == None:
-            self.stop = start
-            self.start = 0
-        else:
-            self.start = start
-            self.stop = stop
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.start == 0:
-            for _ in self.iterator:
-                self.i += 1
-            self.stop = self.i
-
-            if self.start == self.stop:
-                raise StopIteration()
-            self.start += self.step
-            return self.start - self.step
-
-        else:
-            if self.start == self.stop:
-                raise StopIteration()
-
-            self.start += self.step
-            return self.start - self.step
-
-import itertools
-for c in Myislice('12aas',20):
-    print(c,end=' ')
-
-???????????????????????????????????????????????????????????????????????????????
-"""
-
-# Solution 4
-"""
-class Myrepeat:
-    def __init__(self, val, times= None):
-        self.val = val
-        self.times = times
-        self.i = 0
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.times == None:
-            while True:
-                return self.val
-
-        elif self.i < self.times:
-                self.i +=1
-                return self.val
-        else:
-            raise StopIteration
-
-g = Myrepeat(10,5)
-for x in g:
-    print(x, end=' ')
-print()
-
-
-def myrepeat(val, times=None):
-    if not times is None:
-        for i in range(times):
-            yield val
-    else:
-        while True:
-           yield val
-
-g = myrepeat(10,5)
-for x in g:
-    print(x, end=' ')
-
-"""
 
 
 
