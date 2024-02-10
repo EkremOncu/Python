0317f2b78039da750e41515f18eba751275defbb
Ekrem <ekrem.oncu18@gmail.com>
2024-02-10 19:54:53 +0300

    hmw8.py adj

--- ödev/8/hmw8.py
+++ ödev/8/hmw8.py
@@ -1,6 +1,7 @@
 # Solution 
-"""
-d = {'A': {'B': 5, 'C': 7, 'D': 10}, 
+import sys
+
+g = {'A': {'B': 5, 'C': 7, 'D': 10},
      'B': {'A': 5, 'D': 4, 'E': 17, 'G': 20}, 
      'C': {'A': 7, 'D': 6, 'F':9}, 
      'D': {'A': 10, 'B': 4, 'C':6, 'F':12, 'E': 3},
@@ -8,4 +9,20 @@ d = {'A': {'B': 5, 'C': 7, 'D': 10},
      'F': {'C': 9, 'D': 12, 'E': 14}, 
      'G': {'B': 20, 'E': 6, 'F': 10}    
      }
-"""
+
+def get_length(tour):
+     tlen = 0
+     for i in range(len(tour) - 1):
+          ddcit = g.get(tour[i])
+          if ddcit is None:
+               return None
+          nlen = ddcit.get(tour[i+1])
+          if nlen is None:
+               return None
+          tlen += nlen
+
+     return tlen
+
+result = get_length(['A', 'B', 'E', 'G'])
+
+print(result)
